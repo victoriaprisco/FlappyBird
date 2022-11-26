019936f539eb19835f83eba29d676efc73c32b5b
victoriaprisco <89554729+victoriaprisco@users.noreply.github.com>
2022-11-25 23:53:47 -0500

    Update GapDebugApp.java

--- flappytest8/test-5/src/maybeFlappyBird/GapDebugApp.java
+++ flappytest8/test-5/src/maybeFlappyBird/GapDebugApp.java
@@ -2,108 +2,72 @@ package maybeFlappyBird;
 
 import java.awt.Color;
 import java.awt.event.KeyEvent;
-import java.util.LinkedList;
-
 import java.awt.Font;
-import edu.princeton.cs.introcs.StdDraw;
 
 public class GapDebugApp {
-	private static final double RADIUS = 0.01;
-	private static final double REASONABLE_DISTANCE = 0.4;
-	private static Color birdColor = new Color((int) (Math.random() * 255),(int) (Math.random() * 255), (int) (Math.random() * 255));
-	private static LinkedList<Obstacles> obs = new LinkedList<Obstacles>();
-	private static double xBird = 0.2;
-	private static double yBird = 0.5;
-	public static Obstacles test = new Obstacles(0.3, 0.4, 0.05);
+	private final static double RADIUS = 0.01;
+	private static Color birdColor = StdDraw.BLACK;
+	private static double x = 0.2;
+	private static double y = 0.5;
 
-	private static final int SPACE = KeyEvent.VK_SPACE;
-
-	//	 0 - up
-	//	 1 - left
-	//	 2 - down
-	//	 3 - right
-	public static void move(int direction) {
-		if(direction == KeyEvent.VK_UP) {
-			yBird += 0.01;
-		}
-		else if(direction == KeyEvent.VK_LEFT) {
-			xBird -= 0.01;
-		}
-		else if(direction == KeyEvent.VK_DOWN) {
-			yBird -= 0.01;
-		}
-		else if(direction == KeyEvent.VK_RIGHT) {
-			xBird += 0.01;
-		}
-
-		StdDraw.clear(StdDraw.BOOK_LIGHT_BLUE);
-		test.draw();
-		StdDraw.setPenColor(birdColor);
-		StdDraw.filledCircle(xBird, yBird, RADIUS);
-
-		StdDraw.show();
+	public static void move(Obstacle test, int direction) {
+		y = direction == KeyEvent.VK_UP? y + 0.001 : direction == KeyEvent.VK_DOWN ? y - 0.001 : y;
+		x = direction == KeyEvent.VK_LEFT? x - 0.001 : direction == KeyEvent.VK_RIGHT ? x + 0.001 : x;
 	}  
 
-	public static void testBird() {
+	public static void testBird(Obstacle test, Game a) {
 		StdDraw.setPenColor(birdColor);
-		StdDraw.filledCircle(xBird, yBird, RADIUS);
+		StdDraw.filledCircle(x, y, RADIUS);
 		StdDraw.show();
-		while(true) {
+		while(!StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)) {
 			if(StdDraw.isKeyPressed(KeyEvent.VK_LEFT)){
-				move(KeyEvent.VK_LEFT);
+				move(test, KeyEvent.VK_LEFT);
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_RIGHT)){
-				move(KeyEvent.VK_RIGHT);
+				move(test, KeyEvent.VK_RIGHT);
 
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_UP)) {
-				move(KeyEvent.VK_UP);
+				move(test, KeyEvent.VK_UP);
 
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_DOWN)) {
-				move(KeyEvent.VK_DOWN);
+				move(test, KeyEvent.VK_DOWN);
 			}
-			drawAlive();
+			StdDraw.clear(StdDraw.BOOK_LIGHT_BLUE);
+			drawAlive(a);
+			test.draw();
+			StdDraw.setPenColor(birdColor);
+			StdDraw.filledCircle(x, y, RADIUS);
+			StdDraw.show();
 		}
 	}
-	public static void drawAlive() {
-		boolean alive = Game.gameAlive();
-		String errorMessage = "all good!";
-//		if(yBird >= 1 || yBird <= 0) {
-//			alive = false;
-//		}
-//		double[] yCoords = test.getYCoords();
-//		double bottomLimit = yCoords[0];
-//		double topLimit = yCoords[1];
-//		double xCoord = test.getXCoord();
-//		if(Math.abs(xBird - xCoord) <= test.getWidth()) {
-//			if(yBird < bottomLimit) {
-//				errorMessage = "The bird crashed into the bottom bar";
-//			}
-//			if(yBird >= topLimit) {
-//				errorMessage = "The bird crashed into the top bar";
-//			}
-//			alive = false;
-//		}
+	public static void drawAlive(Game a) {
+		a.xBird = x;
+		a.yBird = y;
+		boolean alive = a.gameAlive();
+		String errorMessage;
 		if(alive) {
+			errorMessage = "all good!";
 			StdDraw.setPenColor(StdDraw.GREEN);
 		}
 		else {
+			errorMessage = "bird died";
 			StdDraw.setPenColor(StdDraw.RED);
 		}
 		StdDraw.filledRectangle(0.75, 0.75, 0.2, 0.05);
 		StdDraw.setPenColor();
 		Font font = new Font("Arial", Font.PLAIN, 10);
 		StdDraw.setFont(font);
-		StdDraw.setFont(font);
 		StdDraw.text(0.75, 0.75, errorMessage);
-		StdDraw.show();
 	}
 
 	public static void main(String[] args) {
-		obs.add(test);
+		Game a = new Game();
+		Obstacle test = new Obstacle(0.3, 0.4, 0.2);
+		a.obs.add(test);
 		StdDraw.enableDoubleBuffering();
-		Game.draw();
-		testBird();
+//		a.draw();
+		testBird(test, a);
 	}
 }
